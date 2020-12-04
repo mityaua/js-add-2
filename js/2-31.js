da72717a683ccb9772b7334c521501e1a3b44c93
Dmytro <mityaua@gmail.com>
2020-12-04 11:10:03 +0200

    fix 31 task

--- js/2-31.js
+++ js/2-31.js
@@ -1,12 +1,23 @@
 function findNumber(start, end, divisor) {
   // Пиши код ниже этой строки
-  let result;
   for (let i = start; i < end; i += 1) {
     if (i % divisor === 0) {
-      result = i;
-      break;
+      return (number = i);
     }
   }
-  return result;
   // Пиши код выше этой строки
 }
+
+// Старый вариант решения, до правки бага в боте
+// function findNumber(start, end, divisor) {
+//   // Пиши код ниже этой строки
+//   let result;
+//   for (let i = start; i < end; i += 1) {
+//     if (i % divisor === 0) {
+//       result = i;
+//       break;
+//     }
+//   }
+//   return result;
+//   // Пиши код выше этой строки
+// }
